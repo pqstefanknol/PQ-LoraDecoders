1bb5eaf7ed67e6972a962a95d97ba8e967f6f25f
pqstefanknol <s.knol@purple-q.nl>
2024-06-21 13:58:03 +0200

    removed r311a and added r809a

--- vendor/netvox/payload/r809a.js
+++ vendor/netvox/payload/r809a.js
@@ -3,16 +3,20 @@ function getCfgCmd(cfgcmd){
     1:   "ConfigReportReq",
     129: "ConfigReportRsp",
     2:   "ReadConfigReportReq",
-    130: "ReadConfigReportRsp"
+    130: "ReadConfigReportRsp",
+	144: "Off",
+	145: "On",
+	146: "Toggle",
+	147: "ClearEnergy",
+	148: "ReadCurrentStatus"
   };
   return cfgcmdlist[cfgcmd];
 }
 
 function getDeviceName(dev){
   var deviceName = {
-	2:   "R311A",
-    29:  "R718F",
-	125: "R730F"
+	14: "R809A",
+	115: "R816B"
   };
   return deviceName[dev];
 }
@@ -26,15 +30,23 @@ function getCmdToID(cmdtype){
 	  return 2;
   else if (cmdtype == "ReadConfigReportRsp")
 	  return 130;
+  else if (cmdtype == "Off")
+	  return 144;
+  else if (cmdtype == "On")
+	  return 145;
+  else if (cmdtype == "Toggle")
+	  return 146;
+  else if (cmdtype == "ClearEnergy")
+	  return 147;
+  else if (cmdtype == "ReadCurrentStatus")
+	  return 148;
 }
 
-function getDeviceType(devName){
-  if (devName == "R311A")
-	  return 2;
-  else if (devName == "R718F")
-	  return 29;
-  else if (devName == "R730F")
-	  return 125;
+function getDeviceID(devName){
+  if (devName == "R809A")
+	  return 14;
+  else if (devName == "R816B")
+	  return 115;
 }
 
 function padLeft(str, len) {
@@ -61,35 +73,42 @@ function decodeUplink(input) {
 				data: data,
 			};
 		}
+		
 		data.Device = getDeviceName(input.bytes[1]);
-		if (input.bytes[3] & 0x80)
+		if (input.bytes[2] === 0x01)
 		{
-			var tmp_v = input.bytes[3] & 0x7F;
-			data.Volt = (tmp_v / 10).toString() + '(low battery)';
+			data.OnOff = (input.bytes[3] === 0x00) ? 'OFF' : 'ON';
+			data.Energy = (input.bytes[4]<<24 | input.bytes[5]<<16 | input.bytes[6]<<8 | input.bytes[7]);
+			data.OverCurrentAlarm = (input.bytes[8] === 0x00) ? 'No alarm' : 'Alarm';
+			data.DashCurrentAlarm = (input.bytes[9] === 0x00) ? 'No alarm' : 'Alarm';
+			data.PowerOffAlarm = (input.bytes[10] === 0x00) ? 'No alarm' : 'Alarm';
 		}
 		else
-			data.Volt = input.bytes[3]/10;
-		
-		data.OnOff = input.bytes[4];
+		{
+			data.Vol = (input.bytes[3]<<8 | input.bytes[4]);
+			data.Current = (input.bytes[5]<<8 | input.bytes[6]);
+			data.Power = (input.bytes[7]<<8 | input.bytes[8]);
+		}
 		
 		break;
 		
 	case 7:
+		data.Cmd = getCfgCmd(input.bytes[0]);
 		data.Device = getDeviceName(input.bytes[1]);
-		if (input.bytes[0] === 0x81)
+		if (input.bytes[0] === getCmdToID("ConfigReportRsp"))
 		{
-			data.Cmd = getCfgCmd(input.bytes[0]);
 			data.Status = (input.bytes[2] === 0x00) ? 'Success' : 'Failure';
 		}
-		else if (input.bytes[0] === 0x82)
+		else if (input.bytes[0] === getCmdToID("ReadConfigReportRsp"))
 		{
-			data.Cmd = getCfgCmd(input.bytes[0]);
 			data.MinTime = (input.bytes[2]<<8 | input.bytes[3]);
 			data.MaxTime = (input.bytes[4]<<8 | input.bytes[5]);
-			data.BatteryChange = input.bytes[6]/10;
+			data.CurrentChange = (input.bytes[6]<<8 | input.bytes[7]);
+			data.PowerChange = (input.bytes[8]<<8 | input.bytes[9]);
 		}
-		break;
 		
+		break;	
+
 	default:
       return {
         errors: ['unknown FPort'],
@@ -97,7 +116,6 @@ function decodeUplink(input) {
 	  
     }
           
-	  
 	 return {
 		data: data,
 	};
@@ -106,29 +124,27 @@ function decodeUplink(input) {
 function encodeDownlink(input) {
   var ret = [];
   var devid;
-  var port;
   var getCmdID;
 	  
   getCmdID = getCmdToID(input.data.Cmd);
-  devid = getDeviceType(input.data.Device);
+  devid = getDeviceID(input.data.Device);
 
   if (input.data.Cmd == "ConfigReportReq")
   {
 	  var mint = input.data.MinTime;
 	  var maxt = input.data.MaxTime;
-	  var batteryChg = input.data.BatteryChange * 10;
+	  var curChg = input.data.CurrentChange;
+	  var powChg = input.data.PowerChange;
 	  
-	  port = 7;
-	  ret = ret.concat(getCmdID, devid, (mint >> 8), (mint & 0xFF), (maxt >> 8), (maxt & 0xFF), batteryChg, 0x00, 0x00, 0x00, 0x00);
+	  ret = ret.concat(getCmdID, devid, (mint >> 8), (mint & 0xFF), (maxt >> 8), (maxt & 0xFF), (curChg >> 8), (curChg & 0xFF), (powChg >> 8), (powChg & 0xFF), 0x00);
   }
-  else if (input.data.Cmd == "ReadConfigReportReq")
+  else if ((input.data.Cmd == "ReadConfigReportReq") || (input.data.Cmd == "On") || (input.data.Cmd == "Off") || (input.data.Cmd == "Toggle")  || (input.data.Cmd == "ClearEnergy") || (input.data.Cmd == "ReadCurrentStatus"))
   {
-	  port = 7;
 	  ret = ret.concat(getCmdID, devid, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
-  }  
-    
+  }
+  
   return {
-    fPort: port,
+    fPort: 7,
     bytes: ret
   };
 }  
@@ -137,18 +153,16 @@ function decodeDownlink(input) {
   var data = {};
   switch (input.fPort) {
     case 7:
+		data.Cmd = getCfgCmd(input.bytes[0]);
 		data.Device = getDeviceName(input.bytes[1]);
 		if (input.bytes[0] === getCmdToID("ConfigReportReq"))
 		{
-			data.Cmd = getCfgCmd(input.bytes[0]);
 			data.MinTime = (input.bytes[2]<<8 | input.bytes[3]);
 			data.MaxTime = (input.bytes[4]<<8 | input.bytes[5]);
-			data.BatteryChange = input.bytes[6]/10;
+			data.CurrentChange = (input.bytes[6]<<8 | input.bytes[7]);
+			data.PowerChange = (input.bytes[8]<<8 | input.bytes[9]);
 		}
-		else if (input.bytes[0] === getCmdToID("ReadConfigReportReq"))
-		{
-			data.Cmd = getCfgCmd(input.bytes[0]);
-		}
+
 		break;
 		
     default:
